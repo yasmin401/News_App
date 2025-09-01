a9ca626b04fa31b6429815e48d49621fa89c9c9d
yasmin401 <Yasminm2004@gmail.com>
2025-09-01 06:56:16 +0300

    Add article_model.dart

---
+++ lib/models/article_model.dart
@@ -0,0 +1,19 @@
+class ArticleModel {
+  final String? image;
+  final String title;
+  final String? subTitle;
+
+  const ArticleModel({
+    required this.image,
+    required this.title,
+    required this.subTitle,
+  });
+
+  factory ArticleModel.fromJson(json) {
+    return ArticleModel(
+      image: json['urlToImage'],
+      title: json['title'],
+      subTitle: json['description'],
+    );
+  }
+}
