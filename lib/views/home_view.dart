8d83ed2cfc5d1e68e67c6f64eb12ec4d03082d50
yasmin401 <Yasminm2004@gmail.com>
2025-09-01 07:01:15 +0300

    Update home_view.dart

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:news_app/widgets/categories_list_view.dart';
-
+import 'package:news_app/widgets/news_list_view_builder.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
@@ -32,7 +32,17 @@ class HomeView extends StatelessWidget {
           ],
         ),
       ),
-      body: CategoriesListView()
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: CustomScrollView(
+          physics: BouncingScrollPhysics(),
+          slivers: [
+            SliverToBoxAdapter(child: CategoriesListView()),
+            SliverToBoxAdapter(child: SizedBox(height: 35)),
+            NewsListViewBuilder(category: 'general',),
+          ],
+        ),
+      ),
     );
   }
 }
