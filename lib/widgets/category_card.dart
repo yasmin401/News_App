27b65d55a5b33a4b2be6cb82ec3d0b5e56c4f7d3
yasmin401 <Yasminm2004@gmail.com>
2025-09-01 07:01:40 +0300

    Update category_card.dart

--- lib/widgets/category_card.dart
+++ lib/widgets/category_card.dart
@@ -1,31 +1,46 @@
 import 'package:flutter/material.dart';
 import 'package:news_app/models/category_model.dart';
+import 'package:news_app/views/category_view.dart';
 
 class CategoryCard extends StatelessWidget {
-  const CategoryCard({super.key,required this.categoryModel});
+  const CategoryCard({super.key, required this.categoryModel});
   final CategoryModel categoryModel;
-  
+
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(right: 16),
-      child: Container(
-        height: 110,
-        width: 200,
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            fit: BoxFit.fill,
-            image: AssetImage(categoryModel.image),
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) {
+              return CategoryView(
+                category: categoryModel.categoryName,
+              );
+            },
           ),
-          borderRadius: BorderRadius.circular(10),
-        ),
-        child: Center(
-          child: Text(
-            categoryModel.categoryName,
-            style: const TextStyle(
-              color: Colors.white,
-              fontSize: 18,
-              fontWeight: FontWeight.bold,
+        );
+      },
+      child: Padding(
+        padding: const EdgeInsets.only(right: 16),
+        child: Container(
+          height: 110,
+          width: 200,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              fit: BoxFit.fill,
+              image: AssetImage(categoryModel.image),
+            ),
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: Center(
+            child: Text(
+              categoryModel.categoryName,
+              style: const TextStyle(
+                color: Colors.white,
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
             ),
           ),
         ),
