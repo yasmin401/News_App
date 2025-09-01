15aa98500d1d8654a67ee4b66615216721282acd
yasmin401 <Yasminm2004@gmail.com>
2025-09-01 07:00:23 +0300

    Add news_list_view.dart

---
+++ lib/widgets/news_list_view.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:news_app/models/article_model.dart';
+import 'package:news_app/widgets/news_tile.dart';
+
+class NewsListView extends StatelessWidget {
+  const NewsListView({super.key, required this.articles});
+
+  final List<ArticleModel> articles;
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverList(
+      delegate: SliverChildBuilderDelegate(childCount: articles.length, (
+        context,
+        index,
+      ) {
+        return Padding(
+          padding: const EdgeInsets.only(bottom: 22),
+          child: NewsTile(articleModel: articles[index]),
+        );
+      }),
+    );
+  }
+}
